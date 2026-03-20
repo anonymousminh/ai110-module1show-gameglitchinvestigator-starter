c71df9e8360d2c62355b201e5663733c311a416c
Minh <anhminh7802@gmail.com>
2026-03-19 19:06:54 -0600

    Fix the range for the Hard mode and add type parsing in the check_guess

--- logic_utils.py
+++ logic_utils.py
@@ -5,7 +5,7 @@ def get_range_for_difficulty(difficulty: str):
     if difficulty == "Normal":
         return 1, 100
     if difficulty == "Hard":
-        return 1, 50
+        return 1, 200
     return 1, 100
 
 
@@ -38,7 +38,7 @@ def check_guess(guess, secret):
 
     outcome examples: "Win", "Too High", "Too Low"
     """
-    if guess == secret:
+    if int(guess) == int(secret):
         return "Win", "🎉 Correct!"
 
     try:
